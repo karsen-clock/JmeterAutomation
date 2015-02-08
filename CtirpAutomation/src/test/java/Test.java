9ff608049677f079b03bc60304f8b45aa566ff4b
karsen-clock <testautomation@126.com>
2015-02-08 19:47:03 +0800

    --add testng test report dependency

---
+++ CtirpAutomation/src/test/java/Test.java
@@ -0,0 +1,14 @@
+import org.testng.Reporter;
+import org.testng.ReporterConfig;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello,welcome to use ctrip automation test api");
+		Reporter.log("just a test");
+		Reporter.log("werer", false);
+	}
+
+}
