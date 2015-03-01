853a2fe80107b97c6971f9818c0797b902d4253e
karsen-clock <testautomation@126.com>
2015-03-01 11:10:29 +0800

    --update jsontohashmap function

--- CtirpAutomation/src/test/java/Test1.java
+++ CtirpAutomation/src/test/java/Test1.java
@@ -1,16 +1,36 @@
-import org.testng.Reporter;
-import org.testng.ReporterConfig;
-import org.testng.annotations.Test;
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import com.ctrip.automation.result2map.JsonToHashMap;
+import java.util.HashMap;
+ 
 public class Test1 {
+     
+    public static void main(String[] args) throws IOException
+    {
+                //文件绝对路径改成你自己的文件路径
+        FileReader fr=new FileReader("C:\\Users\\hasee\\Desktop\\json.txt");
+        StringBuilder sb=new StringBuilder();
+        String s="";
+        //可以换成工程目录下的其他文本文件
+        BufferedReader br=new BufferedReader(fr);
+        System.out.println("test");
+        while((s=br.readLine())!=null){
+        	sb.append(s + "\n");
+           
+        }
+        br.close();
+        //String ss=sb.toString();
+        //System.out.println(ss);
+        
+        
+        //HashMap responseMap=new HashMap();
+        //JsonToHashMap js=new JsonToHashMap();
 
-@Test
-		public void first()
-		{
-		assert "edddc".endsWith("c");
-		Reporter.log("just a test");
-		
-		Reporter.log("werer1", false);
-		}
-	}
+        //responseMap=js.parserToMap(ss);
+       // System.out.println(responseMap.get("result"));
 
+    }
+ 
+}
