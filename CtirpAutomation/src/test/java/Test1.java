29d7e69fc78ff1773781cf8947d4d900c5d5bafb
karsen-clock <testautomation@126.com>
2015-03-04 23:10:42 +0800

    --update reg expression

--- CtirpAutomation/src/test/java/Test1.java
+++ CtirpAutomation/src/test/java/Test1.java
@@ -1,36 +1,54 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
+import com.ctrip.automation.result2map.HtmlToHashMap;
 import com.ctrip.automation.result2map.JsonToHashMap;
-import java.util.HashMap;
+
+import java.util.Map;
+
+import org.json.JSONException;
  
 public class Test1 {
      
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args) throws IOException, JSONException
     {
-                //文件绝对路径改成你自己的文件路径
+    	
+    	
+    	
+//    	 String str = "welkes,sdlkwerj";  
+//	        String regex = "(?<=s)(,)(?=s)";  
+//	        String[] strs = str.split(regex);  
+//	        for(int i = 0; i < strs.length; i++) {  
+//	            System.out.printf("strs[%d] = %s%n", i, strs[i]);  
+	            
+	            
+//                文件绝对路径改成你自己的文件路径
         FileReader fr=new FileReader("C:\\Users\\hasee\\Desktop\\json.txt");
         StringBuilder sb=new StringBuilder();
         String s="";
         //可以换成工程目录下的其他文本文件
         BufferedReader br=new BufferedReader(fr);
-        System.out.println("test");
         while((s=br.readLine())!=null){
         	sb.append(s + "\n");
            
         }
         br.close();
-        //String ss=sb.toString();
-        //System.out.println(ss);
+        String ss=sb.toString();
+        System.out.println(ss);
+        
+        JsonToHashMap js=new JsonToHashMap();
+       //System.out.println(((HashMap<String,Object>) ((ArrayList) js.parserToMap(ss).get("payInfo")).get(0)).get("payInfoMain"));
+        System.out.println(js.parserToMap(ss).get("people").getClass());
         
         
-        //HashMap responseMap=new HashMap();
-        //JsonToHashMap js=new JsonToHashMap();
-
-        //responseMap=js.parserToMap(ss);
-       // System.out.println(responseMap.get("result"));
+        
 
     }
  
 }
+
