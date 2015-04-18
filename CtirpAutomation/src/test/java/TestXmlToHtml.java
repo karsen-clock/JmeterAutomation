4e7f233e1cb9dba0f9c85a29e374207c1d429f8a
karsen-clock <testautomation@126.com>
2015-04-18 16:42:50 +0800

    --add code to execute summary report and details report

--- CtirpAutomation/src/test/java/TestXmlToHtml.java
+++ CtirpAutomation/src/test/java/TestXmlToHtml.java
@@ -3,13 +3,16 @@ import com.ctrip.automation.common.XmlToHtml;
 public class TestXmlToHtml {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String xmlFileName = "E:\\GitHubLocal\\Maven-Jmeter\\target\\jmeter\\results\\test.xml";
-        String xslFileName = "E:\\GitHubLocal\\Maven-Jmeter\\src\\test\\resources\\Detail Test Report.xsl";
-        String htmlFileName = "E:\\GitHubLocal\\Maven-Jmeter\\target\\jmeter\\results\\test.html";
+		String xmlFileName = "E:\\GitHubLocal\\Maven-Jmeter\\target\\jmeter\\results\\Testing.xml";
+        String xslFileNameSummary = "E:\\GitHubLocal\\Maven-Jmeter\\src\\test\\resources\\SummaryReport.xsl";
+        String xslFileNameDetails = "E:\\GitHubLocal\\Maven-Jmeter\\src\\test\\resources\\DetailsReport.xsl";
+        String htmlFileNameSummary = "E:\\GitHubLocal\\Maven-Jmeter\\target\\jmeter\\results\\SummaryReport.html";
+        String htmlFileNameDetails = "E:\\GitHubLocal\\Maven-Jmeter\\target\\jmeter\\results\\DetailsReport.html";
         
         XmlToHtml xmlToHtml=new XmlToHtml();
         
-        xmlToHtml.Transform(xmlFileName, xslFileName, htmlFileName);
+        xmlToHtml.Transform(xmlFileName, xslFileNameSummary, htmlFileNameSummary);
+        xmlToHtml.Transform(xmlFileName, xslFileNameDetails, htmlFileNameDetails);
 
 	}
 
