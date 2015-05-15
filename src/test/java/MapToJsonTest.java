eb2f5c2447ab1368b3a68db2a0e2e49c05543a8d
cdzhang <cdzhang@DST42920.cn1.global.ctrip.com>
2015-05-16 00:54:55 +0800

    --add new method to support jsonlite

---
+++ src/test/java/MapToJsonTest.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+import org.json.*;
+
+import com.ctrip.automation.*;
+import com.ctrip.automation.result2map.JsonToHashMap;
+public class MapToJsonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String,String> map=new HashMap<String,String>();
+		map.put("test", "123");
+		map.put("cctv", "");
+		System.out.println(map);
+		JSONObject json=new JSONObject(map);
+		System.out.println(json);
+		JsonToHashMap ps=new JsonToHashMap();
+		
+		try {
+			System.out.println(ps.parserToMap(json.toString(), true));
+			JSONObject json2 = new JSONObject(ps.parserToMap(json.toString(), true));
+			System.out.println(json2.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
