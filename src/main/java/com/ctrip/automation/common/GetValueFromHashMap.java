14cd5e0733be71cb6d80562bc91db21612ef41c0
cdzhang <cdzhang@DST42920.cn1.global.ctrip.com>
2015-06-13 17:18:48 +0800

    --

--- src/main/java/com/ctrip/automation/common/GetValueFromHashMap.java
+++ src/main/java/com/ctrip/automation/common/GetValueFromHashMap.java
@@ -6,16 +6,19 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("unused")
 public class GetValueFromHashMap {
 	
 	Object objectTemp=new Object();	
-	public String getValue(String key,HashMap map)
+	public String getValue(String key,@SuppressWarnings("rawtypes") HashMap map)
 	{ 
 
 		objectTemp=map.get(key);
+		@SuppressWarnings("rawtypes")
 		Iterator iter = map.entrySet().iterator();  
 		while (iter.hasNext() && objectTemp==null) {  
-		    Map.Entry entry = (Map.Entry) iter.next();  
+		    @SuppressWarnings("rawtypes")
+			Map.Entry entry = (Map.Entry) iter.next();  
 		    Object val = entry.getValue();
 		    getChildNode(key,val);
 		} 
@@ -23,11 +26,13 @@ public class GetValueFromHashMap {
 		return objectTemp.toString();
 	}
 	
+	@SuppressWarnings("rawtypes")
 	public void getChildNode(String key,Object val)
 	{
 		 if(val.getClass().getName().equals("java.util.ArrayList"))
 		    {
-		    	List list=(List) val;
+		    	@SuppressWarnings("rawtypes")
+				List list=(List) val;
 		    	for(Iterator ite=list.iterator();ite.hasNext();)
 		    	{
 		    		getValue(key,(HashMap) ite.next());
